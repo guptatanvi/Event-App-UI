8b92b6b4052a4108e8f99f3196ae0238481ec706
guptatanvi <tanvigupta1124@gmail.com>
2017-07-15 18:32:51 +0530

    lots of changes

---
+++ script.js
@@ -0,0 +1,24 @@
+$("#signup-trigger").click(function(){
+  $('#signin').modal('hide');
+  $('#signup').modal('show');
+});
+
+// $("#forgot-trigger").click(function(){
+//   $('#modal-login').modal('hide');
+//   $('#modal-forgot').modal('show');
+// });
+
+$("#signin-trigger").click(function(){
+  $('#signup').modal('hide');
+  $('#signin').modal('show');
+});
+//
+// $("#back-login").click(function(){
+//   $('#modal-forgot').modal('hide');
+//   $('#modal-login').modal('show');
+// });
+//
+// $("#back-join").click(function(){
+//   $('#modal-forgot').modal('hide');
+//   $('#modal-join').modal('show');
+// });
