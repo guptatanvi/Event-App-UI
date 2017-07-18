f048cc51335a64729e1411da5df42dae7cb03aa7
guptatanvi <tanvigupta1124@gmail.com>
2017-07-18 14:35:51 +0530

    delete

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-$("#signup-trigger").click(function(){
-  $('#signin').modal('hide');
-  $('#signup').modal('show');
+$(document).ready(function() {
+$('#category').multiselect({
+nonSelectedText: 'Select Category'
 });
-
-// $("#forgot-trigger").click(function(){
-//   $('#modal-login').modal('hide');
-//   $('#modal-forgot').modal('show');
-// });
-
-$("#signin-trigger").click(function(){
-  $('#signup').modal('hide');
-  $('#signin').modal('show');
 });
-//
-// $("#back-login").click(function(){
-//   $('#modal-forgot').modal('hide');
-//   $('#modal-login').modal('show');
-// });
-//
-// $("#back-join").click(function(){
-//   $('#modal-forgot').modal('hide');
-//   $('#modal-join').modal('show');
-// });
